e3da2ae9a506cc3bba8e271d7974a078bdb4429f
Jerry Wang <jerry.wang@sky.uk>
2023-02-01 09:07:34 +0000

    (test): fix naming for verify example

--- src/test/java/part1/Ex4b_Verify_More_Actions.java
+++ src/test/java/part1/Ex4b_Verify_More_Actions.java
@@ -52,16 +52,14 @@ public class Ex4b_Verify_More_Actions {
 
 
     @Test
-    public void testVerify(@Mock Database database) {
+    public void shouldPerformDatabaseCheck(@Mock Database database) {
         // create and configure mock
         when(database.getUniqueId()).thenReturn(43);
-
 
         // call method testing on the mock with parameter 12
         database.setUniqueId(12);
         database.getUniqueId();
         database.getUniqueId();
-
 
         // now check if method testing was called with the parameter 12
         verify(database).setUniqueId(ArgumentMatchers.eq(12));
